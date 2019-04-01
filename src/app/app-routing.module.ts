75b821e11ac1c04b5f801aa7b1bcc286f00ce7f3
dipankar61 <dipankar61@gmail.com>
2019-04-01 01:49:03 -0400

    add routing and basic form element

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AddTaskComponent} from './add-task/add-task.component';
+import {ViewTaskComponent} from './view-task/view-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'AddTask', component: AddTaskComponent },
+  { path: 'ViewTask',    component: ViewTaskComponent },
+  { path: '',   redirectTo: '/AddTask', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
